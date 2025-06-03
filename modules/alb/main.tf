55ac28859ac64896bcf30418e8a36c8f803b6931
Kyle Hasty <khasty720@gmail.com>
2025-06-02 21:56:22 -0400

    Update ALB

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -59,7 +59,6 @@ resource "aws_lb_target_group" "app" {
     enabled             = true
     interval            = 30
     path                = var.health_check_path
-    port                = "traffic-port"
     healthy_threshold   = 3
     unhealthy_threshold = 3
     timeout             = 5
@@ -81,6 +80,27 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type             = "redirect"
+    redirect {
+      host        = "#{host}"
+      path        = "/#{path}"
+      port        = "443"
+      protocol    = "HTTPS"
+      query       = "#{query}"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
+
+  certificate_arn = var.certificate_arn
 
   default_action {
     type             = "forward"
